72c57f3ebfca21a05c70f405898ffe0abfe4e2fb
Sebastian Jaenicke <sebastian.jaenicke@computational.bio.uni-giessen.de>
2019-11-21 14:51:31 +0100

    dbcan update

--- scripts/dbcan_add_desc.pl
+++ scripts/dbcan_add_desc.pl
@@ -6,25 +6,43 @@ use warnings;
 my $desc = {};
 open(INFO, '<', $ARGV[0]) or die;
 while (my $line = <INFO>) {
-    chomp($line);
-    my @f = split(/\t/, $line);
-    $desc->{$f[0]} = $f[3];
+    if (substr($line,0,1) ne '#') {
+        chomp($line);
+        my @f = split(/\t\s+/, $line);
+        $desc->{$f[0]} = $f[1];
+    }
 }
 close(INFO);
 
 open(HMM, '<', $ARGV[1]) or die;
 while (my $line = <HMM>) {
-    print $line;
+    if (substr($line,0,3) ne 'ACC') {
+        print $line;
+    }
+
     if (substr($line,0,4) eq 'NAME') {
         my ($name) = ($line =~ /^NAME  (.+)$/);
         if ($name =~/\.hmm$/) { substr($name,-4 ) = ''; }
 
-        if ($name eq 'GT2_Cellulose_synt') {
+        # transfer name to accession
+        print "ACC   $name\n";
+
+        if (($name eq 'GT2_Cellulose_synt') or
+            ($name eq 'GT2_Chitin_synth_1') or
+            ($name eq 'GT2_Chitin_synth_2') or
+            ($name eq 'GT2_Glycos_transf_2') or
+            ($name eq 'GT2_Glyco_tranf_2_2') or
+            ($name eq 'GT2_Glyco_tranf_2_3') or
+            ($name eq 'GT2_Glyco_tranf_2_4') or
+            ($name eq 'GT2_Glyco_tranf_2_5') or
+            ($name eq 'GT2_Glyco_trans_2_3')) {
             # entry already has a description
         } else {
 
-            if (exists($desc->{$name})) {
+            if (defined($desc->{$name})) {
                 print "DESC  $name ".$desc->{$name}."\n";
+            } else {
+                print "DESC  $name\n";
             }
         }
     }
